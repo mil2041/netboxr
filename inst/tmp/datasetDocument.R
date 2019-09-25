313e8f9115409ea74f903593818da63ed10502ab
Eric Liu <mil2041@med.cornell.edu>
2019-09-25 14:43:04 -0400

    update code

---
+++ inst/tmp/datasetDocument.R
@@ -0,0 +1,114 @@
+#' network coming with PLOS ONE paper.
+#'
+#' Loading PC2V7_03052015_woHPRD containing 19147 nodes and 2267149 interactions.
+#' Treated as undirected network. 
+#' Removing multiple interactions and loops.
+#' Returning igraph network of 19147 nodes and 2196039 interactions.
+#'
+#' @format A data frame with 19147 nodes and 2196039 interactions:
+#' \describe{
+#'   \item{name}{vertex gene name}
+#'   \item{edges}{interaction types}
+#'   ...
+#' }
+#' @source \url{http://www.pathwaycommons.org/pc2/downloads}
+#' 
+#' @return a data.frame
+#' 
+#' @concept netboxr
+"netbox2010"
+
+#' pathwayCommons v7 data frame.
+#'
+#' Loading PC2V7_03052015_sif containing 16196 nodes and 345026 interactions.
+#'
+#' @format A data frame with 16196 nodes and 309358 interactions:
+#' \describe{
+#'   \item{PARTICIPANT_A}{vertex, gene name}
+#'   \item{INTERACTION_TYPE}{interaction type, transformed binary interaction}
+#'   \item{PARTICIPANT_B}{vertex, gene name}
+#'   \item{INTERACTION_DATA_SOURCE}{interaction database source}
+#'   ...
+#' }
+#' @source \url{http://www.pathwaycommons.org/pc2/downloads}
+#' 
+#' @return a data.frame
+#' 
+#' @concept netboxr
+#"PC2V7_03052015_sif"
+
+#' pathwayCommons v7 data frame.
+#'
+#' Loading PC2V7_03052015_sif containing 16196 nodes and 345026 interactions.
+#'
+#' @format A data frame with 16196 nodes and 336001 interactions:
+#' \describe{
+#'   \item{PARTICIPANT_A}{vertex, gene name}
+#'   \item{INTERACTION_TYPE}{interaction type, transformed binary interaction}
+#'   \item{PARTICIPANT_B}{vertex, gene name}
+#'   \item{INTERACTION_DATA_SOURCE}{interaction database source}
+#'   ...
+#' }
+#' @source \url{http://www.pathwaycommons.org/pc2/downloads}
+#' 
+#' @return a data.frame
+#' 
+#' @concept netboxr
+#"PC2V7_03052015_sif_controlsExpression"
+
+#' pathwayCommons v7 data frame.
+#'
+#' Loading PC2V7_03052015_whole containing 16196 nodes and 345026 interactions.
+#'
+#' @format A data frame with 16196 nodes and 345026 interactions:
+#' \describe{
+#'   \item{PARTICIPANT_A}{vertex, gene name}
+#'   \item{INTERACTION_TYPE}{interaction type, transformed binary interaction}
+#'   \item{PARTICIPANT_B}{vertex, gene name}
+#'   \item{INTERACTION_DATA_SOURCE}{interaction database source}
+#'   ...
+#' }
+#' @source \url{http://www.pathwaycommons.org/pc2/downloads}
+#' 
+#' @return a data.frame
+#' 
+#' @concept netboxr
+#"PC2V7_03052015_sif_signal"
+
+#' pathwayCommons v7 data frame.
+#'
+#' Loading PC2V7_03052015_sif_whole containing 19147 nodes and 2267149 interactions.
+#'
+#' @format A data frame with 19147 nodes and 2267149 interactions:
+#' \describe{
+#'   \item{PARTICIPANT_A}{vertex, gene name}
+#'   \item{INTERACTION_TYPE}{interaction type, transformed binary interaction}
+#'   \item{PARTICIPANT_B}{vertex, gene name}
+#'   \item{INTERACTION_DATA_SOURCE}{interaction database source}
+#'   ...
+#' }
+#' @source \url{http://www.pathwaycommons.org/pc2/downloads}
+#' 
+#' @return a data.frame
+#' 
+#' @concept netboxr
+#"PC2V7_03052015_sif_whole"
+
+#' pathwayCommons v7 data frame.
+#'
+#' Loading bioGridNetworkHomoSapiens34133 containing 19147 nodes and 2267149 interactions.
+#'
+#' @format A data frame with 19147 nodes and 2267149 interactions:
+#' \describe{
+#'   \item{PARTICIPANT_A}{vertex, gene name}
+#'   \item{INTERACTION_TYPE}{interaction type, transformed binary interaction}
+#'   \item{PARTICIPANT_B}{vertex, gene name}
+#'   \item{INTERACTION_DATA_SOURCE}{interaction database source}
+#'   ...
+#' }
+#' @source \url{http://www.pathwaycommons.org/pc2/downloads}
+#' 
+#' @return a data.frame
+#' 
+#' @concept netboxr
+#"bioGridNetworkHomoSapiens34133"
