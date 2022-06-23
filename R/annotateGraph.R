a610f63a88ddfae4e84475c06129b24b79051c7d
sajo25 <sarajovanovska@hotmail.com>
2022-06-23 21:12:27 +0200

    \dontrun implemented only for plots, not entire example

--- R/annotateGraph.R
+++ R/annotateGraph.R
@@ -40,7 +40,6 @@
 #' @author Guanlan Dong, \email{guanlan_dong@g.harvard.edu}
 #'
 #' @examples
-#' \dontrun{
 #' data(pathway_commons_v8_reactome)
 #' interaction_type_color <- read.csv(system.file("interaction_type.color.txt", package = "netboxr"),
 #'                                    header=TRUE, sep="\t", stringsAsFactors=FALSE)
@@ -62,6 +61,7 @@
 #' # As an example, plot both the original and the annotated graphs
 #' ll <- layout_with_fr(results$netboxGraph) # Save the layout for easier comparison
 #' # Plot original graph
+#' \dontrun{
 #' pdf("originalGraph.pdf", width = 50, height = 50)
 #' plot(results$netboxCommunity, results$netboxGraph, layout = ll,
 #'      vertex.size=3)
